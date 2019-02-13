c7698ffaabefcfb7bb75712742e7d1606b7a177e
jcsteven <jcsteven.yu@gmail.com>
2019-02-13 16:54:30 +0800

    Fix unknown charactrer in asfvolt_dev.c and asfvol_dev.h

--- src/asfvolt_dev.h
+++ src/asfvolt_dev.h
@@ -3,14 +3,14 @@
 #include <linux/ioctl.h>
 
 #define ASFVOLT_DEV_IOC_MAGIC 'k'
- 
+
 typedef struct
 {
     unsigned char address;
     unsigned char reg;
-    unsigned char value;	
+    unsigned char value;
 } cpld_data_arg_t;
- 
+
 #define CPLD_DATA_GET_VARIABLES _IOR(ASFVOLT_DEV_IOC_MAGIC, 1, cpld_data_arg_t *)
 #define CPLD_DATA_SET_VARIABLES _IOW(ASFVOLT_DEV_IOC_MAGIC, 2, cpld_data_arg_t *)
 
